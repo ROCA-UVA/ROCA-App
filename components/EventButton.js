69aeeb1671b4fe184a7ba0141efd4d6330ce8ae9
schoi1826 <schoi1826@gmail.com>
2020-03-18 18:35:24 -0400

    Add event buttons

---
+++ components/EventButton.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Button } from 'react-native-elements';
+
+export default class EventButton extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			type: ''
+		}
+	}
+
+	componentDidMount = () => {
+		this.setState({type: this.props.type})
+	}
+
+	render() {
+		if (this.state.type == "instantaneous") {
+			return <InstantaneousEvent />
+		} else if (this.state.type == "durational") {
+			return <DurationalEvent />
+		} else {
+			return <Button title="event type not found" disabled={true} buttonStyle={styles.button} />
+		}
+	}
+}
+
+class InstantaneousEvent extends Component {
+	render() {
+		return (
+			<Button title="instantaneous" buttonStyle={styles.button} />
+		)
+	}
+}
+
+class DurationalEvent extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			active: false,
+			style: styles.buttonActive
+		}
+	}
+
+	handlePress = () => {
+		this.setState({active: !this.state.active});
+	}
+
+	render() {
+		return (
+			<Button title="durational" buttonStyle={[styles.button, this.state.active && this.state.style]} onPress={this.handlePress} />
+		)
+	}
+}
+
+const styles = StyleSheet.create({
+	button: {
+		backgroundColor: 'black',
+		borderRadius: 100,
+		padding: 10,
+		overflow: 'hidden'
+	},
+	buttonActive: {
+		backgroundColor: 'red',
+	}
+});
--- components/RightSide.js
+++ components/RightSide.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 
-import ActivityMenu from './ActivityMenu';
+import EventButton from './EventButton';
 
 export default function RightSide() {
 	return (
 		<View style={{flex: 2, backgroundColor: 'lightblue'}}>
-			<ActivityMenu />
-			<View style={{flex: 9}}>
+			<View style={{flex: 1}}>
+				<EventButton type="instantaneous" />
+				<EventButton type="durational" />
+				<EventButton type="bar" />
 			</View>
 		</View>
 	);
