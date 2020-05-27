82a0556c653deafa84f2548c3b9da2b5b122a5f3
schoi1826 <schoi1826@gmail.com>
2020-05-27 19:41:47 -0400

    Display classroom image

--- components/ClassroomImage.js
+++ components/ClassroomImage.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 
 export default class ClassroomImage extends Component {
 	render() {
 		return (
-			<View style={{flex: 9, backgroundColor: 'white'}}>
-				<Text>ClassroomImage Component</Text>
+			<View style={styles.imageContainer}>
+				<Image 
+					source={require('../assets/images/RICE-130.jpg')}
+					style={styles.image}
+				/>
 			</View>
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+	imageContainer: {
+		flex: 5,
+		backgroundColor: 'white',
+		justifyContent: 'center',
+	},
+	image: {
+		width: '100%',
+		resizeMode: 'contain',
+	}
+})
