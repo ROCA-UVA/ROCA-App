e75c90dbab6bb7bb1df18d43c2dfd7eace13cb81
Helen Lin <30956318+hlin98@users.noreply.github.com>
2020-07-14 12:46:01 -0400

    add square checkbox

--- components/SectionButton.js
+++ components/SectionButton.js
@@ -2,22 +2,55 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 import { Text, TouchableHighlight, View, StyleSheet } from 'react-native';
 import CircleCheckBox, {LABEL_POSITION} from 'react-native-circle-checkbox';  
+import CheckBox from 'react-native-check-box';
+
+// export default class SectionButton extends Component {
+// 	state = { checked: false };
+// 	onToggle = checked => this.setState({ checked });
+
+// 	render() {
+// 		const { checked } = this.state;
+// 		return (
+// 			<View style={{flex: 1, flexDirection: 'col'}}>
+// 				<CircleCheckBox
+//                     checked={checked}
+//                     outerColor={'#FBC02D'}
+//                     innerColor={'#FBC02D'}
+//                     onToggle={this.onToggle}
+//                     labelPosition={LABEL_POSITION.LEFT}
+//                     label="S1"
+// 				/>
+// 			</View>
+// 		);
+// 	}
+// }
 
 export default class SectionButton extends Component {
 	state = { checked: false };
 	onToggle = checked => this.setState({ checked });
 
 	render() {
+		<CheckBox
+			onClick={()=>{
+			this.setState({
+				isChecked:!this.state.isChecked
+			})
+			}}
+			isChecked={this.state.isChecked}
+		/>
 		const { checked } = this.state;
 		return (
-			<View style={{flex: 1, flexDirection: 'col'}}>
-				<CircleCheckBox
-                    checked={checked}
-                    outerColor={'#FBC02D'}
-                    innerColor={'#FBC02D'}
-                    onToggle={this.onToggle}
-                    labelPosition={LABEL_POSITION.LEFT}
-                    label="S1"
+			<View style={{flexDirection: 'row'}}>
+				<CheckBox
+					style={{ flex: 1, paddingLeft: 50}}
+					onClick={()=>{
+					this.setState({
+						isChecked:!this.state.isChecked
+					})
+					}}
+					isChecked={this.state.isChecked}
+					rightText={'1'}
+					checkBoxColor={'gray'}
 				/>
 			</View>
 		);
