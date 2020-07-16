18109e55c8d785f434432e19780e0e10e1edefe9
jiajia Liang <jl9pg@virginia.edu>
2020-07-16 18:09:59 -0400

    vertical layout

--- components/Control.js
+++ components/Control.js
@@ -10,42 +10,44 @@ export default class Control extends Component {
 	render() {
 		return (
 			<View style={{flex: 1, flexDirection: 'col', backgroundColor: 'white',justifyContent: 'space-around',alignItems:'center'}}>
-				<View style={{ flexDirection: 'row',flex:2}}>
+				<View style={{ flexDirection: 'row',flex:1}}>
 					<ControlButton type="Start" />
 					<ControlButton type="Reset" />
 					<CommentForm/>
 				</View>
+                <View style={{flexDirection: 'row',flex:3}}>
 
-                <View style={{flexDirection: 'row',flex:2}}>
-                    <View style={{flexDirection: 'col'}, styles.section}>
-                        <IncrementStudent/>
-                        <SectionButton sectionID='1'/>
+                    <View style={{flexDirection: 'col',flex:2}}>
+                        <View style={styles.section}>
+                            <IncrementStudent/>
+                            <SectionButton sectionID='1'/>
+                        </View>
+                        <View style={styles.section}>
+                            <IncrementStudent/>
+                            <SectionButton sectionID='2'/>
+                        </View>
+                        <View style={styles.section}>
+                            <IncrementStudent/>
+                            <SectionButton sectionID='3'/>
+                        </View>
+                        <View style={styles.section}>
+                            <IncrementStudent/>
+                            <SectionButton sectionID='4'/>
+                        </View>
+                        <View style={ styles.section}>
+                            <IncrementStudent/>
+                            <SectionButton sectionID='5'/>
+                        </View>
                     </View>
-                    <View style={{flexDirection: 'col'}, styles.section}>
-                        <IncrementStudent/>
-                        <SectionButton sectionID='2'/>
-                    </View>
-                    <View style={{flexDirection: 'col'}, styles.section}>
-                        <IncrementStudent/>
-                        <SectionButton sectionID='3'/>
-                    </View>
-                    <View style={{flexDirection: 'col'}, styles.section}>
-                        <IncrementStudent/>
-                        <SectionButton sectionID='4'/>
-                    </View>
-                    <View style={{flexDirection: 'col'}, styles.section}>
-                        <IncrementStudent/>
-                        <SectionButton sectionID='5'/>
+
+                    <View style={{flexDirection: 'col',flex:2,alignItems:'flex-start'}}> 
+                        <SectionEventButton title="Ask Question"/>
+                        <SectionEventButton title="Responds to Instructor" />
+                        <SectionEventButton title="Makes a Prediction" />
+                        <SectionEventButton title="Responds to Student" />
                     </View>
                 </View>
-
-                <View style={{flexDirection: 'row',flex:2}}> 
-                    <SectionEventButton title="Ask Question"/>
-                    <SectionEventButton title="Responds to Instructor" />
-                    <SectionEventButton title="Makes a Prediction" />
-                    <SectionEventButton title="Responds to Student" />
-                </View>
-			</View>
+            </View>
 			
 		);
 	}
@@ -56,7 +58,8 @@ const styles = StyleSheet.create({
         
     },
     section:{
-        flex: 2,
-        alignItems: 'center'
+        alignItems: 'center',
+        flexDirection:'row',
+        justifyContent:'space-evenly'
     }
 });
